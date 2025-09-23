29d1eada54e13ceae6f498656e9bea73164b718e
Santi Chaustre <santiago@chaustre.dev>
2025-09-23 18:31:22 -0500

    Try to fix

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.PROD ? "/corgi-analytics-paw-ty" : ""}>
         <PageTracker />
         <Routes>
           <Route path="/" element={<Index />} />
