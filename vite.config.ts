fbf2d1559bb4bbdb57a76917f2f57421b730e19f
Santi Chaustre <santiago@chaustre.dev>
2025-09-23 18:28:11 -0500

    Issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/corgi-analytics-paw-ty/' : '/',
+  base: process.env.NODE_ENV === 'production' || mode === 'production' ? '/corgi-analytics-paw-ty/' : '/',
   server: {
     host: "::",
     port: 8080,
